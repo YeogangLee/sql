9917546c3e1efd6de28c6762ae2d910b295f0824
DESKTOP-2SBVSE1\PC-14 <coffee357722@gmail.com>
2021-05-22 17:51:38 +0900

    sqld 목차

---
+++ sqld_prc.sql
@@ -0,0 +1,11 @@
+-- 테이블명 변경
+-- 방법1. ALTER TABLE OLD RENAME TO NEW;
+ALTER TABLE DEPT RENAME TO DEFT;
+-- 방법2. RENAME OLD TO NEW;
+RENAME DEFT TO DEPT;
+
+COMMIT;
+
+-- GROUPING 함수 설명
+https://oracle-base.com/articles/misc/rollup-cube-grouping-functions-and-grouping-sets
+
